a5a2ef04a5336f6fb3cbffc1bb5c69aa5ac60b3d
e <495469217@qq.com>
2019-10-13 22:34:27 +0800

    求众数

--- leetcode/169.求众数.java
+++ leetcode/169.求众数.java
@@ -1,3 +1,7 @@
+import java.util.Stack;
+
+import javax.swing.text.DefaultStyledDocument.ElementSpec;
+
 /*
  * @lc app=leetcode.cn id=169 lang=java
  *
@@ -7,8 +11,15 @@
 // @lc code=start
 class Solution {
     public int majorityElement(int[] nums) {
-        
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (stack.empty() || stack.peek() == nums[i]) {
+                stack.push(nums[i]);
+            } else {
+                stack.pop();
+            }
+        }
+        return stack.pop();
     }
 }
 // @lc code=end
-
