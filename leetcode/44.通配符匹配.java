89c3008136e4a1dd992e65380971699b0f8865e5
lzw <li495469217@163.com>
2019-12-09 16:03:25 +0800

    commit

--- leetcode/44.通配符匹配.java
+++ leetcode/44.通配符匹配.java
@@ -6,22 +6,50 @@
 
 // @lc code=start
 class Solution {
+    Boolean[][] book;
+
     public boolean isMatch(String s, String p) {
-        boolean[][] book = new boolean[s.length() + 1][p.length() + 1];
-        book[0][0] = false;
-        for (int i = 1; i <= p.length(); i++)
-            book[i][0] = p.charAt(i - 1) == '*' && book[i - 1][0];
-        for (int i = 1; i <= p.length(); i++) {
-            for (int j = 1; j <= s.length(); j++) {
-                if (p.charAt(i-1) == '*') {
-                    book[i][j] = book[i - 1][j] || book[i][j - 1];
-                }
-                if (s.charAt(j-1) == p.charAt(i-1) || p.charAt(i-1) == '?') {
-                    book[i][j] = book[i - 1][j - 1];
+        book = new Boolean[p.length() + 1][s.length() + 1];
+        book[0][0] = true;
+        return isMatch(s, p, 0, 0);
+    }
+
+    public boolean isMatch(String s, String p, int i, int j) {
+        if (book[i][j] != null) {
+            return book[i][j];
+        }
+        if (p.length() == i) {
+            book[i][j] = s.length() == j;
+            return book[i][j];
+        }
+        if (p.charAt(0) == '*') {
+            if (p.length() == 1) {
+                book[p.length()][s.length()] = true;
+                return book[p.length()][s.length()];
+            }
+            if (p.length() > 1 && p.charAt(1) == '*') {
+                book[p.length()][s.length()] = isMatch(s, p.substring(1));
+                return book[p.length()][s.length()];
+            }
+            for (int i = s.length() - 1; i >= 0; i--) {
+                if (p.charAt(1) != s.charAt(i) && p.charAt(1) != '?')
+                    continue;
+                if (isMatch(s.substring(i), p.substring(1))) {
+                    book[p.length()][s.length()] = true;
+                    return book[p.length()][s.length()];
                 }
             }
+            return false;
         }
-        return book[s.length()][p.length()];
+        if (s.length() == 0) {
+            book[p.length()][s.length()] = false;
+            return book[p.length()][s.length()];
+        }
+        if (p.charAt(0) == s.charAt(0) || p.charAt(0) == '?') {
+            book[p.length()][s.length()] = isMatch(s.substring(1), p.substring(1));
+            return book[p.length()][s.length()];
+        }
+        return false;
     }
 }
 // @lc code=end
