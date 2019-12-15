68549fe116ba13b5df1dd9b419a58db64baad7bd
lzw <li495469217@163.com>
2019-12-15 23:45:05 +0800

    commit

---
+++ leetcode/94.二叉树的中序遍历.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import sun.tools.tree.ArrayAccessExpression;
+
+/*
+ * @lc app=leetcode.cn id=94 lang=java
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if(root==null) return new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        list.addAll(inorderTraversal(root.left));
+        list.add(root.val);
+        list.addAll(inorderTraversal(root.right));
+        return list;
+    }
+}
+// @lc code=end
