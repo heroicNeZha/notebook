2e6bfcf1cf28470037a2d0ace0bf4d6c8f64985c
lzw <li495469217@163.com>
2019-12-17 23:56:46 +0800

    commit

--- leetcode/101.对称二叉树.java
+++ leetcode/101.对称二叉树.java
@@ -1,4 +1,7 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
+
 
 /*
  * @lc app=leetcode.cn id=101 lang=java
@@ -14,13 +17,24 @@ import java.util.Stack;
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         if (root == null)
-            return true;
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode present = root.left;
-        while (present != null) {
-            stack.add(present);
-            present = present.left;
+            return false;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        Stack<Integer> stack = new Stack<>();
+        queue.offer(root.left);
+        queue.offer(root.right);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node != null)
+                if (stack.peek() == node.val)
+                    stack.pop();
+                else
+                    stack.push(node.val);
+            else if (stack.peek() == 0)
+                stack.pop();
+            else
+                stack.push(0);
         }
+        return stack.isEmpty();
     }
 }
 // @lc code=end
