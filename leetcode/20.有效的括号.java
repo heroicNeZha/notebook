281d237d3e7ada5177358cdd9555711f1aa3c7db
lzw <li495469217@163.com>
2020-02-12 19:39:00 +0800

    学习

--- leetcode/20.有效的括号.java
+++ leetcode/20.有效的括号.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 /*
  * @lc app=leetcode.cn id=20 lang=java
  *
@@ -7,8 +9,38 @@
 // @lc code=start
 class Solution {
     public boolean isValid(String s) {
-        
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            if (stack.empty()) {
+                stack.push(s.charAt(i));
+            } else {
+                switch (s.charAt(i)) {
+                case '}':
+                    if (stack.peek() == '{')
+                        stack.pop();
+                    else
+                        stack.push(s.charAt(i));
+                    break;
+                case ']':
+                    if (stack.peek() == '[')
+                        stack.pop();
+                    else
+                        stack.push(s.charAt(i));
+                    break;
+                case ')':
+                    if (stack.peek() == '(')
+                        stack.pop();
+                    else
+                        stack.push(s.charAt(i));
+                    break;
+                default:
+                    stack.push(s.charAt(i));
+                }
+            }
+        }
+        if (stack.empty())
+            return true;
+        return false;
     }
 }
 // @lc code=end
-
--- 学习/frontend/vue.md
+++ 学习/frontend/vue.md
@@ -22,6 +22,32 @@
     </script>
     ```
 2. 模板语言
+   1. v-html="template" 会崩溃??
+   2. v-bind:href="url" 绑定属性,可以用:代替
+   3. v-on:click="count()" 绑定事件,可以用@代替
+    ```html
+    <div id="div1" v-html="template">
+        {{msg}}
+        {{countNum}}<br>
+        <a :href="url">百度</a>
+        <button @click="count()">click</button>
+    </div>
+    <script>
+        new Vue({
+            el: '#div1',
+            data: {
+                url: 'http://baidu.com',
+                msg: 'hello vue <br>',
+                countNum: 0
+            },
+            methods: {
+                count: function () {
+                    this.countNum++
+                }
+            }
+        })
+    </script>
+    ```
 ### 基本概念
 ### 组件
 ### 常用API
