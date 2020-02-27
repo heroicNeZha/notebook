523409e349f91e7642931c3547fef38e3c0661c0
lzw <li495469217@163.com>
2020-02-27 21:32:20 +0800

    学习

---
+++ leetcode/203.移除链表元素.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=203 lang=java
+ *
+ * [203] 移除链表元素
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode(int x) { val = x; } }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        while (head != null && head.val == val) {
+            head = head.next;
+        }
+        if (head == null)
+            return null;
+        ListNode p = head;
+        while (p.next != null) {
+            if (p.next.val == val) {
+                p.next = p.next.next;
+            } else {
+                p = p.next;
+            }
+        }
+        return head;
+    }
+}
+// @lc code=end
