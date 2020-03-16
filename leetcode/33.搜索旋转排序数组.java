37cf481e802e108aeb368a1be5f236fb88712668
lzw <li495469217@163.com>
2020-03-16 23:55:03 +0800

    学习

--- leetcode/33.搜索旋转排序数组.java
+++ leetcode/33.搜索旋转排序数组.java
@@ -7,26 +7,34 @@
 // @lc code=start
 class Solution {
     public int search(int[] nums, int target) {
+        if (nums.length == 0)
+            return -1;
         return helper(nums, 0, nums.length - 1, target);
     }
 
     public int helper(int[] nums, int start, int end, int target) {
         int ans = -1;
-        int mid = start + end / 2;
+        int mid = (start + end) / 2;
         if (start == end) {
             return nums[start] == target ? start : -1;
         }
-
-        if (nums[0] < nums[mid]) {
-            if (target < nums[mid]) {
+        if (start > end)
+            return -1;
+        if (nums[mid] == target) {
+            return mid;
+        }
+        if (nums[0] <= nums[mid]) {//左边升序
+            if (target < nums[mid] && target >= nums[0]) {
+                ans = helper(nums, start, mid - 1, target);
+            } else {
+                ans = helper(nums, mid + 1, end, target);
+            }
+        } else {//右边升序
+            if (target > nums[mid] && target < nums[0]) {
+                ans = helper(nums, mid + 1, end, target);
+            } else {
                 ans = helper(nums, start, mid - 1, target);
             }
-        }
-
-        if (target < nums[mid] && target >= nums[start]) {
-            ans = helper(nums, start, mid - 1, target);
-        } else if (target > nums[mid] || target < nums[0]) {
-            ans = helper(nums, mid + 1, end, target);
         }
         return ans;
     }
---
+++ leetcode/39.组合总和.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=39 lang=java
+ *
+ * [39] 组合总和
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+
+    }
+}
+// @lc code=end
+
