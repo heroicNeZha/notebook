58057bb406272f671757399d69880ae81141ed01
lzw <li495469217@163.com>
2020-03-18 20:30:14 +0800

    学习

--- leetcode/39.组合总和.java
+++ leetcode/39.组合总和.java
@@ -1,3 +1,4 @@
+
 /*
  * @lc app=leetcode.cn id=39 lang=java
  *
@@ -5,10 +6,31 @@
  */
 
 // @lc code=start
-class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+import java.util.*;
 
+class Solution {
+    List<List<Integer>> result = new ArrayList<>();
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        if (candidates == null)
+            return result;
+        Stack<Integer> stack = new Stack<>();
+        dfs(candidates, stack, 0, target);
+        return result;
+    }
+
+    public void dfs(int[] candidates, Stack<Integer> pre, int begin, int target) {
+        if (target == 0) {
+            result.add(new ArrayList<>(pre));
+            return;
+        }
+        for (int i = begin; i < candidates.length; i++) {
+            if (target >= candidates[i]) {
+                pre.push(candidates[i]);
+                dfs(candidates, pre,i, target - candidates[i]);
+                pre.pop();
+            }
+        }
     }
 }
 // @lc code=end
-
